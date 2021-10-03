be7bd77821517db8243d059c051345b8925c2492
Duc <62656512+lukamix@users.noreply.github.com>
2021-10-04 00:50:27 +0700

    Add Sign Up Scene

--- App.js
+++ App.js
@@ -8,10 +8,8 @@ import { StatusBar } from 'expo-status-bar';
 
 const styles = require('./assets/styles/appstyle.js');
 const subloginstyles = require('./assets/styles/subloginstyle.js');
+const signupstyles = require('./assets/styles/signupstyle.js');
 
-function onPressLoginButton() {
-  
-}
 function onPressSignUpButton() {
   console.log("signup nek");
 }
@@ -99,7 +97,7 @@ function HomeScreen({navigation}) {
               <Text style={styles.loginbuttonText}>Login</Text>
           </View>
         </TouchableOpacity>
-        <TouchableOpacity onPress={onPressSignUpButton}> 
+        <TouchableOpacity onPress={() => navigation.navigate('SignUpSubScene')}> 
           <View style={styles.SignUpButton}>
               <Text style={styles.signupbuttonText}>Sign Up</Text>
           </View>
@@ -186,6 +184,75 @@ function LoginSubScene({navigation}) {
     </View>
   );
 }
+//Next Scene that will appear if you click on the SignUp button in Home Screen
+function SignUpSubScene({navigation}){
+  const [signupphonenumber, setText] = useState("");
+  return(
+    <View style ={signupstyles.container}>
+      <View style={subloginstyles.header}>
+        <TouchableOpacity style={styles.back_button_image} activeOpacity={0.5} onPress={() => navigation.navigate('Home')}>
+            <Image
+              source={require('./assets/images/common/back-button.png')}
+              style={subloginstyles.back_button_image}
+            />
+        </TouchableOpacity>
+        <Text style={subloginstyles.login_title}>
+            Tạo tài khoản
+        </Text>
+      </View>
+      <View style={subloginstyles.sublogin_text_view}>
+        <Text style={subloginstyles.login_sub_title}>
+          Vui lòng nhập số điện thoại của bạn để tạo tài khoản mới
+        </Text>
+      </View>
+      <View style={subloginstyles.login_field}>
+        <TextInput
+          style={subloginstyles.textInput}
+          value={signupphonenumber}
+          placeholder="Số Điện Thoại"
+          keyboardType="numeric"
+          onChangeText={(value) => setText(value)}
+        />
+        <TouchableOpacity
+            style={subloginstyles.closeButtonParent}
+            onPress={() => setText("")}
+          >
+            <Image
+              style={subloginstyles.closeButton}
+              source={require("./assets/images/common/close.png")}
+            />
+          </TouchableOpacity>
+      </View>
+      <View style={subloginstyles.login_field}>
+        <TextInput 
+          style={subloginstyles.textInput}
+          value={signupphonenumber}
+          placeholder="Họ Và Tên"
+          onChangeText={(value) => setText(value)}
+        />
+        <TouchableOpacity
+            style={subloginstyles.closeButtonParent}
+            onPress={() => setText("")}
+          >
+            <Image
+              style={subloginstyles.closeButton}
+              source={require("./assets/images/common/close.png")}
+            />
+          </TouchableOpacity>
+      </View>
+      <View style={subloginstyles.next_button_field}>
+        <TouchableOpacity
+            style={subloginstyles.next_button}
+          >
+            <Image
+              style={subloginstyles.next_button_image}
+              source={require("./assets/images/common/next-grey-button.png")}
+            />
+          </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
 
 const Stack = createNativeStackNavigator();
 
@@ -197,6 +264,7 @@ function App() {
   }} initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="LoginSubScene" component={LoginSubScene} />
+        <Stack.Screen name="SignUpSubScene" component={SignUpSubScene}/>
       </Stack.Navigator>
     </NavigationContainer>
   );

fb9e09477d19d97fb0a067a4d4d93d26c45b1ae9
Greywolf-edu <ngthlong2109.edu@gmail.com>
2021-10-12 19:49:20 +0700

    color scale

--- Constants/Constants.js
+++ Constants/Constants.js
@@ -1,12 +1,18 @@
 const URI = 'http://192.168.1.101:8000/api/v1/';
-const MAIN_COLOR = '#F048B8';
-const SUB_COLOR = '#E078BD';
-const BLUE_COLOR = '#46BBFF';
-const RED_COLOR = '#FF2424';
+const MAIN_COLOR = '#D782BA';   //Middle Purple
+const SUB_COLOR = '#EEB1D5';    //Cotton Candy
+const BLUE_COLOR = '#388697';   //Metallic Seaweed
+const RED_COLOR = '#CC2936';    //Madder Lake
+const YELLOW_COLOR = '#E2C290'; //Gold Crayola
+const GREY_COLOR = '#736B60';   //Dim Gray
+const DARK_COLOR = '#0B0014';   //Xiketic
 module.exports = {
     URI,
     MAIN_COLOR,
     SUB_COLOR,
     BLUE_COLOR,
     RED_COLOR,
+    YELLOW_COLOR,
+    GREY_COLOR,
+    DARK_COLOR,
 }

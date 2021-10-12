f24369b899a154ac11859b273ed21ff312dd7706
Duc <62656512+lukamix@users.noreply.github.com>
2021-10-13 01:45:29 +0700

    update TimelineScreen...
    Add lần lượt UI vào nhưng mà nặng quá, từ từ :)

--- Constants/Constants.js
+++ Constants/Constants.js
@@ -3,10 +3,12 @@ const MAIN_COLOR = '#F048B8';
 const SUB_COLOR = '#E078BD';
 const BLUE_COLOR = '#46BBFF';
 const RED_COLOR = '#FF2424';
+const GREY_COLOR = '#B4B4B4';
 module.exports = {
     URI,
     MAIN_COLOR,
     SUB_COLOR,
     BLUE_COLOR,
     RED_COLOR,
+    GREY_COLOR,
 }
